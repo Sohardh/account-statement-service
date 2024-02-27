6d04a593ae60b81df5f9d262119c6412ef9d7e28
Sohardh Chobera <sohardhchobera684@gmail.com>
2024-02-27 23:48:42 +0530

    initial commit

---
+++ src/test/java/com/sohardh/accountstatementservice/AccountStatementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sohardh.accountstatementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountStatementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
