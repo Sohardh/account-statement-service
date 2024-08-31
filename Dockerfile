19a981d004403d3f2020e2386028e347ace0a0a9
Sohardh Chobera <sohardhchobera684@gmail.com>
2024-08-31 16:38:16 +0530

    add libs for chrome driver

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,31 @@
 FROM openjdk:21-jdk-slim as build
+
+# Install necessary packages for Chrome
+RUN apt-get update && apt-get install -y \
+    wget \
+    gnupg \
+    unzip \
+    fonts-liberation \
+    libasound2 \
+    libatk-bridge2.0-0 \
+    libatk1.0-0 \
+    libgconf-2-4 \
+    libgtk-3-0 \
+    libnspr4 \
+    libnss3 \
+    xdg-utils \
+    libx11-xcb1 \
+    libxcomposite1 \
+    libxdamage1 \
+    libxfixes3 \
+    libxrandr2 \
+    libgbm-dev \
+    libpango1.0-0 \
+    libcups2 \
+    libdrm2 \
+    libxshmfence1 \
+    libnss3-dev
+
 VOLUME /tmp
 ARG JAR_FILE
 COPY ${JAR_FILE} app.jar
--- src/main/java/com/sohardh/account/config/WebDriverConfig.java
+++ src/main/java/com/sohardh/account/config/WebDriverConfig.java
@@ -1,18 +1,31 @@
 package com.sohardh.account.config;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 public class WebDriverConfig {
 
   @Bean
-  public WebDriver getChromeDriver(){
-    WebDriverManager.chromedriver().setup();
-    return new ChromeDriver();
+  public WebDriver getChromeDriver() {
+    try {
+      var options = new ChromeOptions();
+      options.addArguments("--headless");
+      options.addArguments("--no-sandbox");
+      options.addArguments("--disable-dev-shm-usage");
+      options.addArguments("--disable-extensions");
+      WebDriverManager.chromedriver().setup();
+      return new ChromeDriver(options);
+    } catch (Exception e) {
+      log.error("Failed to initialize chrome driver", e);
+      throw e;
+    }
   }
 
 }
